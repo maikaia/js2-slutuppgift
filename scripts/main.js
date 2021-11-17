adf63b5bab3021e3554a353a9774e2209b548901
Nolle <you@example.com>
2021-11-17 16:45:17 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#description1").hide()
+    $("#description2").hide()
+
+    $("#moreInfo1").click(function(){
+        $("#description1").toggle()
+    })
+    $("#moreInfo2").click(function(){
+        $("#description2").toggle()
+    })
+
+    $(".expandingImg").hover(function() {
+        $(this).css("width", "160px");
+    }, function() {
+        $(this).css("width", "150px");
+    })
+})
